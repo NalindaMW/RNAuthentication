1cf04dc21f0d1f41fade8ec1063bfc4638cacbb6
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-06-26 11:18:47 +0530

    + Added AuthContext + AuthContext used in signin and signup screens + AuthContextProvider wrapped in App

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { login } from "../utils/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authContext = useContext(AuthContext);
+
   async function signinHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authContext.token(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed",
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from "../utils/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authContext = useContext(AuthContext);
+
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authContext.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed",
